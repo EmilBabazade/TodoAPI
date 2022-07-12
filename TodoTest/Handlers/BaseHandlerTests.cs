2f367ae0566c27a4b37f7c53b20966dddbcaebed
emilbabazade20@gmail.com <emilbabazade29@gmail.com>
2022-07-12 15:17:13 +0400

    register new user tests

---
+++ TodoTest/Handlers/BaseHandlerTests.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using TodoAPI.Data;
+using TodoAPI.Helpers;
+
+namespace TodoTest.Handlers
+{
+    public class BaseHandlerTests
+    {
+        protected DataContext _dataContext;
+        protected IMapper _mapper;
+
+        [OneTimeSetUp]
+        public void SetupDb()
+        {
+            DbContextOptions<DataContext>? options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            _dataContext = new DataContext(options);
+            _dataContext.Database.EnsureCreated();
+
+            MapperConfiguration? mapFactory = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutoMapperProfiles());
+            });
+            _mapper = mapFactory.CreateMapper();
+        }
+
+        [TearDown]
+        public async Task ClearData()
+        {
+            _dataContext.Users.RemoveRange(await _dataContext.Users.ToListAsync());
+            await _dataContext.SaveChangesAsync();
+        }
+
+    }
+}
---
+++ TodoTest/Handlers/Users/RegisterUserTests.cs
@@ -0,0 +1,80 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TodoAPI.Services.jwt;
+using TodoAPI.Todo.Users;
+using TodoAPI.Todo.Users.DTOs;
+using TodoAPI.Todo.Users.Handlers;
+
+namespace TodoTest.Handlers.Users
+{
+    public class RegisterUserTests : BaseHandlerTests
+    {
+        private RegisterHandler _handler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            IConfigurationRoot? config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                 .AddEnvironmentVariables()
+                 .Build();
+            _handler = new RegisterHandler(_dataContext, new JWTService(config));
+        }
+
+        // register should register
+        [Test]
+        public async Task AddingNewUserShouldAddNewUserToUsersTable()
+        {
+            // Arrange
+            RegisterDTO registerDTO = new()
+            {
+                Email = "new@user.com",
+                Name = "bob",
+                Password = "123"
+            };
+
+            // Act
+            await _handler.Handle(new RegisterCommand(registerDTO), new CancellationToken());
+            System.Collections.Generic.List<UserEntity>? usersInDB = await _dataContext.Users.ToListAsync();
+            UserEntity? registeredUser = await _dataContext.Users.SingleOrDefaultAsync(u => u.Email == registerDTO.Email);
+
+            // Assert
+            registeredUser.Should().NotBeNull();
+            usersInDB.Count.Should().Be(1);
+        }
+
+        // registering user with existing email should throw BadRequestException
+        [Test]
+        public async Task AddingNewUserWithExistingEmailShouldThrowException()
+        {
+            // Arrange
+            RegisterDTO registerDTO = new()
+            {
+                Email = "new@user.com",
+                Name = "bob",
+                Password = "123"
+            };
+            string? type = "";
+
+            // Act
+            await _handler.Handle(new RegisterCommand(registerDTO), new CancellationToken());
+            try
+            {
+                await _handler.Handle(new RegisterCommand(registerDTO), new CancellationToken());
+            }
+            catch (Exception ex)
+            {
+                type = ex.GetType()?.FullName;
+            }
+
+
+            // Assert
+            type.Should().Be("TodoAPI.Errors.Exceptions+BadRequestException");
+        }
+    }
+}
