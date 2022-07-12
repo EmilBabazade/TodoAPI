5a76d57066ed76144866031bace382cfccded012
emilbabazade20@gmail.com <emilbabazade29@gmail.com>
2022-07-12 13:57:25 +0400

    Automapper

---
+++ TodoAPI/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using TodoAPI.Helpers;
+
+namespace TodoAPI.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
+            services.AddMediatR(typeof(Program));
+            return services;
+        }
+    }
+}
---
+++ TodoAPI/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace TodoAPI.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+
+        }
+    }
+}
